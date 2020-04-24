6c352115b45e9d93e6c6b6395c7516a6eb91a708
Sheepsftw <tefsheep@gmail.com>
2020-04-23 17:05:05 -0700

    add project firebase-shade

---
+++ firebase-shade/src/main/java/com/mysal/queuerooms/firebase_shade/App.java
@@ -0,0 +1,13 @@
+package com.mysal.queuerooms.firebase_shade;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
